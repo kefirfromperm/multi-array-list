92bccfb6f2eb53ca43d7c22d8f634578b20ac164
Vitalii Samolovskikh <kefir@perm.ru>
2013-02-03 21:52:58 +0600

    new speed test

--- test-list/src/org/kefirsf/list/SpeedTest.java
+++ test-list/src/org/kefirsf/list/SpeedTest.java
@@ -10,30 +10,133 @@ import java.util.List;
  */
 public class SpeedTest {
     public static void main(String[] args) {
-        test(new ArrayList<Integer>());
-        test(new DoubleArrayList<Integer>());
-        test(new LinkedList<Integer>());
-    }
+        System.out.println("Warming");
+        test(new ArrayList<Integer>(), 1000);
+        test(new DoubleArrayList<Integer>(), 1000);
+        test(new LinkedList<Integer>(), 1000);
 
-    public static void test(List<Integer> list) {
-        list.clear();
         System.out.println();
-        System.out.println("Collection: " + list.getClass().getName());
+        System.out.println("DoubleArrayList");
 
-        test(list, 1000);
-        test(list, 10000);
-        test(list, 50000);
-        test(list, 100000);
-        test(list, 200000);
-        test(list, 300000);
-        test(list, 400000);
-        test(list, 500000);
-        test(list, 600000);
-        test(list, 700000);
-        test(list, 800000);
-        test(list, 900000);
-        test(list, 1000000);
+        testRead(new DoubleArrayList<Integer>(), 1000000);
+        testRead(new DoubleArrayList<Integer>(), 2000000);
+        testRead(new DoubleArrayList<Integer>(), 3000000);
+        testRead(new DoubleArrayList<Integer>(), 4000000);
+        testRead(new DoubleArrayList<Integer>(), 5000000);
+        testRead(new DoubleArrayList<Integer>(), 6000000);
+        testRead(new DoubleArrayList<Integer>(), 7000000);
+        testRead(new DoubleArrayList<Integer>(), 8000000);
+        testRead(new DoubleArrayList<Integer>(), 9000000);
+        testRead(new DoubleArrayList<Integer>(), 10000000);
+
+        System.out.println();
+        System.out.println("ArrayList");
+
+        testRead(new ArrayList<Integer>(), 1000000);
+        testRead(new ArrayList<Integer>(), 2000000);
+        testRead(new ArrayList<Integer>(), 3000000);
+        testRead(new ArrayList<Integer>(), 4000000);
+        testRead(new ArrayList<Integer>(), 5000000);
+        testRead(new ArrayList<Integer>(), 6000000);
+        testRead(new ArrayList<Integer>(), 7000000);
+        testRead(new ArrayList<Integer>(), 8000000);
+        testRead(new ArrayList<Integer>(), 9000000);
+        testRead(new ArrayList<Integer>(), 10000000);
     }
+
+    private static void testFiltrations() {
+        System.out.println();
+        System.out.println("DoubleArrayList");
+
+        testFiltration(new DoubleArrayList<Integer>(), 1000000);
+        testFiltration(new DoubleArrayList<Integer>(), 2000000);
+        testFiltration(new DoubleArrayList<Integer>(), 3000000);
+        testFiltration(new DoubleArrayList<Integer>(), 4000000);
+        testFiltration(new DoubleArrayList<Integer>(), 5000000);
+        testFiltration(new DoubleArrayList<Integer>(), 6000000);
+        testFiltration(new DoubleArrayList<Integer>(), 7000000);
+        testFiltration(new DoubleArrayList<Integer>(), 8000000);
+        testFiltration(new DoubleArrayList<Integer>(), 9000000);
+        testFiltration(new DoubleArrayList<Integer>(), 10000000);
+
+        System.out.println();
+        System.out.println("LinkedList");
+
+        testFiltration(new LinkedList<Integer>(), 1000000);
+        testFiltration(new LinkedList<Integer>(), 2000000);
+        testFiltration(new LinkedList<Integer>(), 3000000);
+        testFiltration(new LinkedList<Integer>(), 4000000);
+        testFiltration(new LinkedList<Integer>(), 5000000);
+        testFiltration(new LinkedList<Integer>(), 6000000);
+        testFiltration(new LinkedList<Integer>(), 7000000);
+        testFiltration(new LinkedList<Integer>(), 8000000);
+        testFiltration(new LinkedList<Integer>(), 9000000);
+        testFiltration(new LinkedList<Integer>(), 10000000);
+    }
+
+    private static void fullTest() {
+        System.out.println();
+        System.out.println("ArrayList");
+
+        test(new ArrayList<Integer>(), 20000);
+        test(new ArrayList<Integer>(), 40000);
+        test(new ArrayList<Integer>(), 60000);
+        test(new ArrayList<Integer>(), 80000);
+        test(new ArrayList<Integer>(), 100000);
+        test(new ArrayList<Integer>(), 120000);
+        test(new ArrayList<Integer>(), 140000);
+        test(new ArrayList<Integer>(), 160000);
+        test(new ArrayList<Integer>(), 180000);
+        test(new ArrayList<Integer>(), 200000);
+
+        System.out.println();
+        System.out.println("DoubleArrayList");
+
+        test(new DoubleArrayList<Integer>(), 20000);
+        test(new DoubleArrayList<Integer>(), 40000);
+        test(new DoubleArrayList<Integer>(), 60000);
+        test(new DoubleArrayList<Integer>(), 80000);
+        test(new DoubleArrayList<Integer>(), 100000);
+        test(new DoubleArrayList<Integer>(), 120000);
+        test(new DoubleArrayList<Integer>(), 140000);
+        test(new DoubleArrayList<Integer>(), 160000);
+        test(new DoubleArrayList<Integer>(), 180000);
+        test(new DoubleArrayList<Integer>(), 200000);
+
+        System.out.println();
+        System.out.println("LinkedList");
+
+        test(new LinkedList<Integer>(), 20000);
+        test(new LinkedList<Integer>(), 40000);
+        test(new LinkedList<Integer>(), 60000);
+        test(new LinkedList<Integer>(), 80000);
+        test(new LinkedList<Integer>(), 100000);
+        test(new LinkedList<Integer>(), 120000);
+        test(new LinkedList<Integer>(), 140000);
+        test(new LinkedList<Integer>(), 160000);
+        test(new LinkedList<Integer>(), 180000);
+        test(new LinkedList<Integer>(), 200000);
+    }
+
+    private static void testRead(List<Integer> list, int size) {
+        System.out.print(size);
+        System.out.print(" ");
+
+        long begin = System.currentTimeMillis();
+        fill(list, size);
+        long end = System.currentTimeMillis();
+        System.out.print(end-begin);
+        System.out.print(" ");
+
+        begin = System.currentTimeMillis();
+        indexRead(list);
+        end = System.currentTimeMillis();
+        System.out.print(end - begin);
+        System.out.print(" ");
+
+        System.out.println();
+    }
+
 
     private static void test(List<Integer> list, int size) {
         System.out.print(size);
@@ -54,6 +157,30 @@ public class SpeedTest {
         begin = System.currentTimeMillis();
         indexRead(list);
         end = System.currentTimeMillis();
+        System.out.print(end - begin);
+        System.out.print(" ");
+
+        begin = System.currentTimeMillis();
+        filtration(list);
+        end = System.currentTimeMillis();
+        System.out.print(end - begin);
+        System.out.print(" ");
+
+        begin = System.currentTimeMillis();
+        removeFirst(list);
+        end = System.currentTimeMillis();
+        System.out.print(end - begin);
+
+        System.out.println();
+    }
+
+    private static void testFiltration(List<Integer> list, int size) {
+        System.out.print(size);
+        System.out.print(" ");
+
+        long begin = System.currentTimeMillis();
+        fill(list, size);
+        long end = System.currentTimeMillis();
         System.out.print(end-begin);
         System.out.print(" ");
 
